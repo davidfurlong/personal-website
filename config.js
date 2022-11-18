7782d8ef5dd50f49ca806248a10a5c664a0f9daa
David Furlong <dvfurlong@googlemail.com>
2022-11-18 19:03:36 +0100

    fix: add space

--- config.js
+++ config.js
@@ -36,7 +36,7 @@ module.exports = {
   author: {
     name: "David Furlong",
     photo: "/photo.png",
-    bio: "Building discove.xyz. Prev:Founder & CTO at Deedmob, a platform empowering citizens, charities and companies to do more good. Forbes 30 under 30 Social Entrepreneurship. Ex CS & Phil Oxford University",
+    bio: "Building discove.xyz. Prev: Founder & CTO at Deedmob, a platform empowering citizens, charities and companies to do more good. Forbes 30 under 30 Social Entrepreneurship. Ex CS & Phil Oxford University",
     contacts: {
       email: "dvfurlong@gmail.com",
       twitter: "davidvfurlong",
